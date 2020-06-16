6b28507af2080baefb848b3efd0c8373e3b81004
garethfriend <garethfriend@hotmail.com>
2020-06-16 23:27:32 +0100

    initial commit

---
+++ js/object.js
@@ -0,0 +1,120 @@
+export default class FatigueStrength {
+    constructor(tempAltStress = null, tempMeanStress = null, tempRRatio = null, tempMaxStress = null, _peakIn = false, _kt = 1, _halfPkPk = true) {
+        // I want the constructor to simply add these first couple as properties
+        // I started using _ to differentiate between the property and the passed value but then had to swap it around 
+        this.peakIn = _peakIn;
+        this.kt = _kt;
+        this.halfPkPk = _halfPkPk;
+              
+        // this ensures that regardless of how it is inputted half-waveform amplitude is what is stored
+        if (!this.halfPkPk && Number.isFinite(tempAltStress)) {
+            tempAltStress /= 2;
+        }
+
+        // main calculation
+        [this.altStress, 
+            this.meanStress, 
+            this.rRatio, 
+            this.maxStress] = this.CalculateStresses(tempAltStress, tempMeanStress, tempRRatio, tempMaxStress)
+    }
+
+    // METHODS
+    CalculateStresses (alt, mean, r, max) {
+        // Throws an error if more or less than two values are passed or tempMeanStress and tempRRatio are the pair because it is an 
+        // unsolvable combination. 
+        if (this._CountNumbers(alt, mean, r, max) === 2 && !(Number.isFinite(mean) && Number.isFinite(r))) {
+            // For a given pair if both are numbers use thier functions, if neither are numbers use the oppsoite pairs 
+            // functions, after two rounds only one valid pair remains.
+            console.log(`Alt: ${alt} Mean: ${mean} R: ${r} Max: ${max}`);
+            if (this._CountNumbers(alt, mean) === 2) {
+                r = this._RFromMeanAlt(mean, alt);
+                max = this._MaxFromAltMean(alt, mean);
+            } else if (this._CountNumbers(alt, mean) === 0) {
+                alt = this._AltFromMaxR(max, r);
+                mean = this._MeanFromMaxAlt(max, alt);
+            } else if (this._CountNumbers(mean, max) === 2) {
+                alt = this._AltFromMaxMean(max, mean);
+                r = this._RFromMeanAlt(mean, alt);
+            } else if (this._CountNumbers(mean, max) === 0) {
+                max = this._MaxFromAltR(alt, r);
+                mean = this._MeanFromMaxR(max, r);
+            } else {
+                r = this._RFromAltMax(alt, max);
+                mean = this._MeanFromMaxR(max, r);
+            }
+            console.log(`Output: Alt: ${alt} Mean: ${mean} R: ${r} Max: ${max}`);
+             return [alt, mean, r, max]
+        } else {
+            throw 'you must pass exactly two parameters which are not meanStress and rRatio';    
+        }
+        
+    }
+
+    // maths functions that the above re-uses in different combinations
+    _RFromMeanAlt (mean, alt) {
+        return (mean - alt)/(mean + alt);
+    }
+    _MeanFromMaxR (max, r) {
+        return max * (1 + r) / 2;
+    }
+    _AltFromMaxR (max, r) {
+        return max * (1 - r) / 2;
+    }
+    _RFromAltMax(alt, max) {
+        return 1 - ((2 * alt) / max);
+    }
+    _MaxFromAltR (alt, r) {
+        return (2 * alt) / (1 - r);
+    }
+    _AltFromMaxMean (max, mean) {
+        return max - mean;
+    }
+    _MeanFromMaxAlt (max, alt) {
+        return max - alt;
+    }
+    _MaxFromAltMean (alt, mean) {
+        return alt + mean;
+    }
+    _CountNumbers(paramA, paramB, paramC = null, paramD = null) {
+       return [paramA, paramB, paramC, paramD].filter(p => Number.isFinite(p)).length;
+    }
+
+    // methods used for scaling alt stress and recalculating others, percent given as decimal. Could use calculate function 
+    // again but more computationally efficient not to go through if statements again as we know which pair is being passed
+    // and we don't need all the cases.
+    
+    // used to apply saftey factors to R-M diagrams i.e. 50% on alternating stress for a given mean value
+    ScaleAltFixMean(scalingFactor) {
+        this.altStress *= scalingFactor;
+        this.rRatio = this._RFromMeanAlt(this.meanStress, this.altStress);
+        this.maxStress = this._MaxFromAltMean(this.altStress, this.meanStress);
+    }
+    
+    // used to translate factors of alternating stress into new max stresses for a given R ratio i.e. locking R ratio rather 
+    // than mean.  
+    ScaleAltFixR(scalingFactor) {
+        this.altStress *= scalingFactor;
+        this.maxStress = this._MaxFromAltR(this.altStress, this.rRatio);
+        this.meanStress = this._MeanFromMaxR(this.maxStress, this.rRatio);
+    }
+    
+    // used to translate differences in test conditions i.e. Max + R pairs into alt-mean space
+    ScaleMaxFixR(scalingFactor) {
+        this.maxStress *= scalingFactor;
+        this.altStress = this._AltFromMaxR(this.maxStress, this.rRatio);
+        this.meanStress = this._MeanFromMaxAlt(this.maxStress, this.altStress);
+    }
+
+    // used to 
+    ScaleMeanFixAlt(scalingFactor) {
+        this.meanStress *= scalingFactor;
+        this.rRatio = this._RFromMeanAlt(this.meanStress, this.altStress);
+        this.maxStress = this._MaxFromAltMean(this.altStress, this.meanStress);
+    }
+
+    // GETTERS 
+    get altStress_pkpk () {
+        return this.altStress * 2;
+    }
+    
+}
---
+++ js/script.js
@@ -0,0 +1,125 @@
+import FatigueStrength from "./object.js"
+// window.FatigueStrength = FatigueStrength;
+// let F1 = new FatigueStrength(250, 0, null, null); // should return R = -1 and max = 250
+// console.log(F1)
+// // let F2 = new FatigueStrength(null, 200, 0.1, null); // should not be possible
+// // console.log(F2)
+// let F3 = new FatigueStrength(null, null, 0.1, 300); // should be alt = 135 and mean = 165
+// console.log(F3)
+// let F4 = new FatigueStrength(250, null, null, 300); // should be a negative R and mean = 50
+// console.log(F4)
+// let F5 = new FatigueStrength(100, null, 0.1, null); 
+// console.log(F5)
+// let F6 = new FatigueStrength(200, null, 0.1, null, true, 1, false); 
+// console.log(F6)
+// let F7 = new FatigueStrength(null, 150, null, 300); // should be alt = 150 and R = 0
+// console.log(F7)
+// let F8 = new FatigueStrength(null, 300, null, 600, true, 2); // should be alt = 150 and R = 0
+// console.log(F8)
+
+// UI class: Handle UI tasks
+class UI {
+    static displayResults() {
+        const results = Store.getResults();
+        Results.forEach((result) => UI.addResultToList(result));
+    }
+
+    static addResultToList(result) {
+        const list = document.querySelector('#'));
+        const row = document.createElement('tr');
+        row.innerHTML = '<td>${result.altStress}</td><td>${result.meanStress}</td><td>${result.rRatio}</td><td>${result.maxStress}</td><td><a href="#" class="delete">X</a></td>';
+        list.appendChild(row);    
+    }
+    static deleteResult(el) {
+        if(el.classlist.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `enter a class name here ${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('x');
+        const form = document.querySelector('x');
+        container.insertBefore(div, form);
+        // vanish in 3 seconds
+        setTimeout(() => document.querySelector('x').remove(), 3000);
+    }
+
+    static clearFields() {
+        document.querySelector('x').value = '';
+    }
+
+}
+
+//  Store Class: Handle Storage
+class Store {
+    static getResults() {
+        let results;
+        if(localStorage.getItem('results') === null) {
+            results = [];
+        } else {
+            results = JSON.parse(localStorage.getItem('results'));
+        }
+        return results;
+    }
+    static addResult(result) {
+        const results = Store.getResults();
+        results.push(result);
+        localStorage.setItem('results', JSON.stringify(results));
+    }
+    static removeResult(id) {
+        const results = Store.getResults();
+        results.forEach((result, index) => {
+            if (result.id === id) {
+                results.splice(index, 1);
+            }
+        });    
+        localStorage.setItem('results', JSON.stringify(results));
+    }
+}
+
+// Event: Display
+document.addEventListener('DOMContentLoaded', UI.displayResults);
+
+// Event: Add
+document.querySelector('#').addEventListener('submit', (e) => {
+    // prevent actual submit
+    e.preventDefault();
+    
+    // get form values
+    const x = document.querySelector('x').value;
+    const x = document.querySelector('x').value;
+    const x = document.querySelector('x').value;
+    const x = document.querySelector('x').value;
+    const x = document.querySelector('x').value;
+    const x = document.querySelector('x').value;
+    const x = document.querySelector('x').value;
+
+    // validate
+    if (validation) {
+        UI.showAlert('message', 'class');
+    } else {
+
+        // instantiate result
+        const result = new FatigueStrength(alt, mean, r, max, peak, kt, pkpk);
+
+        // add result to UI
+        UI.addResultToList(result);
+
+        // add result to store
+        Store.addResult(result);
+    }
+
+
+});
+
+// Event: remove
+document.querySelector('x-list').addEventListener('click', (e) => {
+    // remove from UI
+    UI.deleteResult(e.target);
+
+    // remove from store
+    Store.removeResult(e.target.parentElement.previousElementSibling.textContent);
+});
